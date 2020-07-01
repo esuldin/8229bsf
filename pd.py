9102e60ec8e216ffd37d37ae6e1bd34c4352e5d6
wmouse <>
2020-07-01 13:45:19 +0300

    Add 'polarity' option

--- pd.py
+++ pd.py
@@ -5,6 +5,10 @@ class SamplerateError(Exception):
     pass
 
 
+class SignalPolarityError(Exception):
+    pass
+
+
 class Decoder(srd.Decoder):
     api_version = 3
     id = '8229bsf'
@@ -20,6 +24,8 @@ class Decoder(srd.Decoder):
         {'id': 'sdo', 'name': 'SDO', 'desc': 'Data'},
     )
     options = (
+        {'id': 'polarity', 'desc': 'Signal polarity', 'default': 'active-low',
+            'values': ('active-low', 'active-high')},
         {'id': 'key_num', 'desc': 'Key number', 'default': 8,
             'values': (8, 16)},
     )
@@ -38,6 +44,11 @@ class Decoder(srd.Decoder):
         self.key_num = None
         self.out_ann = None
 
+        self.active_signal = None
+        self.passive_signal = None
+        self.front_edge = None
+        self.back_edge = None
+
         self.timeout_samples_num = None
 
         self.dv_block_ss = None
@@ -52,6 +63,19 @@ class Decoder(srd.Decoder):
         self.out_ann = self.register(srd.OUTPUT_ANN)
         self.key_num = self.options['key_num']
 
+        if self.options['polarity'] == 'active-high':
+            self.active_signal = 'h'
+            self.passive_signal = 'l'
+            self.front_edge = 'r'
+            self.back_edge = 'f'
+        elif self.options['polarity'] == 'active-low':
+            self.active_signal = 'l'
+            self.passive_signal = 'h'
+            self.front_edge = 'f'
+            self.back_edge = 'r'
+        else:
+            raise SignalPolarityError('Unexpected type of signal polarity')
+
     def reset(self):
         self.dv_block_ss = None
         self.tw_block_ss = None
@@ -61,26 +85,31 @@ class Decoder(srd.Decoder):
         if not self.timeout_samples_num:
             raise SamplerateError('Cannot decode without samplerate.')
 
+        if not self.active_signal or not self.passive_signal or not self.front_edge or not self.back_edge:
+            raise SignalPolarityError('Polarity of signal is not set')
+
         while True:
-            self.wait({0: 'h', 1: 'f'})
+            self.wait({0: self.passive_signal, 1: self.front_edge})
             self.dv_block_ss = self.samplenum
 
-            self.wait({0: 'h', 1: 'r'})
+            self.wait({0: self.passive_signal, 1: self.back_edge})
             self.put(self.dv_block_ss, self.samplenum, self.out_ann, [0, ['Data valid', 'DV']])
             self.tw_block_ss = self.samplenum
 
-            self.wait([{0: 'f', 1: 'h'}, {0: 'f', 1: 'f'}])
+            self.wait([{0: self.front_edge, 1: self.passive_signal}, {0: self.front_edge, 1: self.front_edge}])
             self.put(self.tw_block_ss, self.samplenum, self.out_ann, [1, ['Tw', 'Tw']])
             self.bt_block_ss = self.samplenum
 
             keys_pressed = list()
 
             for i in range(self.key_num):
-                (scl, sdo) = self.wait({0: 'r'})
-                sdo = 0 if sdo else 1
+                (scl, sdo) = self.wait({0: self.back_edge})
 
-                if sdo:
+                if (sdo == 1 and self.active_signal == 'h') or (sdo == 0 and self.active_signal == 'l'):
                     keys_pressed.append(str(i + 1))
+                    sdo = 1
+                else:
+                    sdo = 0
 
                 self.wait([{0: 'f'}, {'skip': self.timeout_samples_num}])
                 self.put(self.bt_block_ss, self.samplenum, self.out_ann, [2, ['Bit: %d' % sdo, '%d' % sdo]])
